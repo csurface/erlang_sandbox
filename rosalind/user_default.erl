4f8d9ec217b29722ce6c9fd5abba637ef3c0bb95
Charlie Surface <csurface@bandwidth.com>
2012-10-11 01:01:43 -0400

    overlap graphs and finding a shared motif

--- rosalind/user_default.erl
+++ rosalind/user_default.erl
@@ -1,11 +1,129 @@
 -module(user_default).
 
--export([
-    count_nuc/1, rna_trans/1, rev_comp/1, count_point_mut/2, pro_trans/1,
-    find_motif/2, cons_and_prof/1, gc_content/1
-]).
+-compile(export_all).
 
 -define(FORMAT_LIST(L), [io:format("~p ", [P]) || P <- L], io:format("~n")).
+
+% Finding a Shared Motif
+% http://rosalind.info/problems/lcs/
+% Find one longest common substring of all given strings.
+% MinLength gives a performance improvement.
+find_shared_motif(Filename, MinLength) ->
+    {ok, Handle} = file:open(Filename, read),
+    Strings = read_strings(Handle),
+    file:close(Handle),
+    find_shared_motif_strings(Strings, MinLength).
+
+read_strings(Handle) ->
+    lists:reverse(read_strings(Handle, [])).
+
+read_strings(Handle, Acc) ->
+    case file:read_line(Handle) of
+        {ok, String} ->
+            case string:strip(String) of
+                S when length(S) > 0 ->
+                    read_strings(Handle, [string:strip(S, right, $\n)|Acc]);
+                _ ->
+                    Acc
+            end;
+        eof ->
+            Acc;
+        Error ->
+            Error
+    end.
+
+find_shared_motif_strings(Strings, MinLength) ->
+    % Sort ascending so that we select motifs from the shortest string.
+    SortAsc = fun(A, B) -> length(A) =< length(B) end,
+    Sorted = lists:sort(SortAsc, Strings),
+    % Reverse the motifs so that we are searching for longest ones first.
+    Motifs = lists:reverse(all_substrings(lists:nth(1, Sorted), MinLength)),
+    io:format("Found ~p motifs~n", [length(Motifs)]),
+    Remainder = lists:nthtail(1, Sorted),
+    find_in_all(Motifs, Remainder).
+
+% Return a subset of strings in Motifs that exist in all Strings.
+find_in_all([], _) ->
+    not_found;
+find_in_all([Motif|T], Strings) ->
+    case is_motif_in_strings(Motif, Strings) of
+        true ->
+            Motif;
+        _ ->
+            find_in_all(T, Strings)
+    end.
+
+is_motif_in_strings([], []) ->
+    false;
+is_motif_in_strings(_Motif, []) ->
+    true;
+is_motif_in_strings(Motif, [S|Tail]) ->
+    case find_motif(Motif, S) of
+        [] ->
+            false;
+        L when is_list(L), length(L) > 0 ->
+            is_motif_in_strings(Motif, Tail)
+    end.
+
+all_substrings(S, MinLength) ->
+    all_substrings(S, length(S), MinLength, []).
+
+all_substrings(_S, 0, _MinLength, Acc) ->
+    Acc;
+all_substrings(_S, Length, MinLength, Acc) when Length < MinLength ->
+    Acc;
+all_substrings(S, Length, MinLength, Acc) when length(S) =:= Length ->
+    all_substrings(S, Length-1, MinLength, [S|Acc]);
+all_substrings(S, Length, MinLength, Acc) ->
+    Acc2 = find_all_substrings(S, Length, lists:seq(1, length(S) - Length + 1), Acc),
+    all_substrings(S, Length-1, MinLength, Acc2).
+
+find_all_substrings(_S, _Length, [], Acc) ->
+    Acc;
+find_all_substrings(S, Length, [Position|Tail], Acc) ->
+    Sub = lists:sublist(S, Position, Length),
+    find_all_substrings(S, Length, Tail, [Sub|Acc]).
+
+% Overlap Graphs
+% http://rosalind.info/problems/grph/
+overlap_graphs(Filename, K) ->
+    Fasta = read_fasta_file(Filename),
+    AL = adjacency_list(Fasta, K, length(Fasta), []),
+    [io:format("~s ~s~n", [N1, N2]) || {N1, N2} <- AL].
+
+adjacency_list(_, _, 0, Acc) ->
+    Acc;
+adjacency_list(Fasta, K, N, Acc) ->
+    {Name, String} = lists:nth(N, Fasta),
+    Remainder = remainder(Fasta, N),
+    Acc2 = is_adjacent({Name, String}, K, Remainder, []),
+    adjacency_list(Fasta, K, N-1, lists:flatten([Acc2|Acc])).
+
+is_adjacent(_, _, [], Acc) ->
+    lists:reverse(Acc);
+is_adjacent({Name1, String1}, K, [{Name2, String2}|Tail], Acc) ->
+    N = length(String1),
+    Suffix = lists:nthtail(N-K, String1),
+    Acc2 = case lists:prefix(Suffix, String2) of
+        true ->
+            [{Name1, Name2}|Acc];
+        _ ->
+            Acc
+    end,
+    is_adjacent({Name1, String1}, K, Tail, Acc2).
+
+remainder([_|T], 1) ->
+    T;
+remainder(L, N) ->
+    A = lists:sublist(L, N-1),
+    B = lists:nthtail(N, L),
+    lists:flatten([A|B]).
+
+read_fasta_file(Filename) ->
+    {ok, Handle} = file:open(Filename, read),
+    Fasta = read_fasta(Handle),
+    file:close(Handle),
+    Fasta.
 
 % GC Content
 % http://rosalind.info/problems/gc/
@@ -156,7 +274,10 @@ occurs_at_pos(Symbol, Strings, N) ->
 find_motif(T, S) when is_list(T), is_list(S) ->
     find_motif(list_to_binary(T), list_to_binary(S));
 find_motif(T, S) when is_binary(T), is_binary(S), size(T) =< size(S) ->
-    L = find_motif_at(T, S, 1, []),
+    find_motif_at(T, S, 1, []).
+
+find_motif_formatted(T, S) ->
+    L = find_motif(T, S),
     ?FORMAT_LIST(L).
 
 % Call do_find_motif for every value in S.
